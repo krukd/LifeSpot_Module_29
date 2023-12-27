d22356f053ffa28befa8b1cbdd00d5dc47abbe2f
krukd <krukd87@gmail.com>
2023-12-27 13:11:49 +0800

    Module_29_HW

---
+++ LifeSpot/Static/JS/testing.js
@@ -0,0 +1,60 @@
+﻿// Получим коллекцию всех элементов страницы
+//let elements = document.getElementsByTagName('*');
+// Выведем результат в уведомление
+//alert(`Количество элементов на странице:  ${elements.length}`);
+
+/*
+* Функция для проверки и сохранения  данных пользователя
+* Также блокирует доступ к сайту лицам, не подтвердившим свой возраст
+*
+* */
+
+
+//function handleSession() {
+//    // создадим объект Map для хранения сессии
+//    let session = new Map();
+//    // Сохраним UserAgent
+//    session.set("userAgent", window.navigator.userAgent);
+//    // Запросим возраст пользователя и тоже сохраним
+//    session.set("age", prompt("Пожалуйста, введите ваш возраст"));
+//    // Проверка на возраст и сохранение сессии
+//    if (session.get("age") >= 18) {
+//        let startDate = new Date().toLocaleString();
+
+//        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+//        session.set("startDate", startDate)
+//    }
+//    else {
+//        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+//        window.location.href = "http://www.google.com"
+//    }
+//    // возвращаем объект сессии
+//    return session;
+//}
+
+function handleSession() {
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+}
+
+function checkAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    if (session.get("age") >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+    }
+}
+
+// Логирование сессии (объявлено через expression)
+let sessionLog = function logSession() {
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
